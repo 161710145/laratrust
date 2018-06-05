77a400e029c427de0e29d704d5185fe71a8d556c
161710145 <161710145@smkassalaambandung.sch.id>
2018-06-05 15:23:58 +0700

    first commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,28 @@
+<!-- MENU SIDEBAR-->
+        <aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                    <img src="/assets/admin/images/icon/logo.png" alt="Cool Admin" />
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li class="active has-sub">
+                            
+                                    <a href="{{ route('absen.index') }}">Absen</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('siswa.index') }}">siswa</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('kelas.index') }}">kelas</a>
+                                </li>
+                       
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <!-- END MENU SIDEBAR-->
